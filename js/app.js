55e48b9e3b07c321c0fb210f153dc94a4d1e9b10
critter1996 <109566820+critter1996@users.noreply.github.com>
2024-01-13 15:26:06 -0500

    Added notes to variables

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 //Global Variables
-let employees = [];
+let employees = []; // empty array that will hold values from the API
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
-email, location, phone, dob &noinfo &nat=US`
-const gridContainer = document.querySelector(".grid-container");
-const overlay = document.querySelector(".overlay");
-const modalContainer = document.querySelector(".modal-content");
-const modalClose = document.querySelector(".modal-close");
+email, location, phone, dob &noinfo &nat=US` //string literal that stores the url of the API, complete with desired options
+const gridContainer = document.querySelector(".grid-container"); //stores the DOM element that is the container for the employees
+const overlay = document.querySelector(".overlay"); //stores the DOM element that acts as an overlay for the modal
+const modalContainer = document.querySelector(".modal-content"); //stores the DOM element that is a container for the modal info
+const modalClose = document.querySelector(".modal-close"); //stores the DOM element that is the modal’s close button
